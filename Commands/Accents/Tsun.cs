0b4db299c7bfb1da3b200d93b3de48e83b0891be
MMFF8 <42034901+MMFF8@users.noreply.github.com>
2018-08-05 03:18:02 +0800

    Add files via upload

---
+++ Commands/Accents/Tsun.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using System.IO;
+
+namespace Karl_Narx_V2.Modules
+{
+    public class Tsun : ModuleBase<SocketCommandContext>
+    {
+        [Command("Tsundere mode")]
+        public async Task Tsunnn()
+        {
+            string cmode = File.ReadLines("Mode.txt").First();
+            if (cmode != "Tsundere")
+            {
+                using (StreamWriter newTask = new StreamWriter("Mode.txt", false))
+                {
+                    newTask.WriteLine("Tsundere");
+                }
+                await Context.Channel.SendMessageAsync("I-I'm not doing this cause you told me to... B-Baka!");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("I-I'm not a Tsundere!!! KONO BAKA BAKA!!!");
+            }
+        }
+    }
+}
---
+++ Commands/Accents/Yoda.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using System.IO;
+
+namespace Karl_Narx_V2.Modules
+{
+    public class Yoda : ModuleBase<SocketCommandContext>
+    {
+        [Command("Yoda mode")]
+        public async Task adoY()
+        {
+            string cmode = File.ReadLines("Mode.txt").First();
+            if (cmode != "Yoda")
+            {
+                using (StreamWriter newTask = new StreamWriter("Mode.txt", false))
+                {
+                    newTask.WriteLine("Yoda");
+                }
+                await Context.Channel.SendMessageAsync("Talk backwards now, I will");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("What do you mean? Normal, my speaking is.");
+            }
+        }
+    }
+}
