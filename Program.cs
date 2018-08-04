e06971dc2bc1973278c08ea982b0c6ce8532fb94
MMFF8 <42034901+MMFF8@users.noreply.github.com>
2018-08-05 03:37:59 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Karl_Narx_V2
             _client = new DiscordSocketClient(new DiscordSocketConfig { });
             Global.Client = _client;
             new Commandler(_client);
-            await _client.LoginAsync(Discord.TokenType.Bot, "NDYyNjQwNTg2MDQyNjM4MzY3.Dh5Ylw.GvT2DRceHapPVy1b5CGkd9B9WVk");
+            await _client.LoginAsync(Discord.TokenType.Bot, "Get your own token M8");
             await _client.StartAsync();
             _client.Ready += Timerer.StartTimer;
             await Task.Delay(-1);
